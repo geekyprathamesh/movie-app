57ed55a437d5fe9483244e0432828fd2015cb4e2
prathamesh Talavdekar <prathamesh1073@gmail.com>
2023-03-24 20:57:39 +0530

    Add action types and action creators

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
   componentDidMount(){
@@ -12,17 +13,13 @@ class App extends React.Component {
     })
     //make a api call
     //dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    })
+    store.dispatch(addMovies(data));
 
-    console.log('STATE', this.props.store.getState());
+     console.log('STATE', this.props.store.getState());
   }
 
   render() {
     const movies = this.props.store.getState();
-    console.log('RENDERED')
     return (
       <div className="App">
         <Navbar />
