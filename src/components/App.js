b4ec02cb5fb4de1cffcf92876e9ed7b49ed2d1fb
prathamesh Talavdekar <prathamesh1073@gmail.com>
2023-03-25 20:41:13 +0530

    Changing the state to add favouraties as well

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,8 @@ class App extends React.Component {
   }
 
   render() {
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();
+    console.log('RENDER', this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -30,7 +31,7 @@ class App extends React.Component {
           </div>
 
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movie-${index}`}/>
             ))}
           </div>
